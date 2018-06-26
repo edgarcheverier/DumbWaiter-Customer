00f905b299cbc552be626e8e4b5d0a55b3bcd789
edgarcheverier <edgarcheverier@gmail.com>
2018-06-26 12:40:38 +0200

    User Page, User Image, vuex shopping state

--- src/containers/Welcome/Welcome.vue
+++ src/containers/Welcome/Welcome.vue
@@ -1,50 +1,71 @@
 <template>
   <div>
-    <nav>
-      <div class="nav-wrapper">
-        <h6 class="brand-logo center">Dumbwaiter</h6>
-      </div>
-    </nav>
+    <welcomeNav 
+      :user-image="userImage" 
+      :go-to-user="goToUser"/>
     <div class="foodDrinksContainer">
-      <div class="food">
+      <div class="buttonsLabel">
         <button 
-          class="btn-floating btn-large waves-effect waves-light cyan" 
+          class="btn-floating btn-large waves-effect waves-light cyan z-depth-4" 
           @click="handleClickFood('Food')"><i class="material-icons">local_dining</i></button>
+        <label class="labelClass">Food</label>
       </div>
-      <div class="drinks">
+      <div class="buttonsLabel">
         <button 
-          class="btn-floating btn-large waves-effect waves-light cyan" 
+          class="btn-floating btn-large waves-effect waves-light cyan z-depth-4" 
           @click="handleClickFood('Drinks')"><i class="material-icons">local_bar</i></button>
+        <label class="labelClass">Drinks</label>
       </div>
     </div>
     <div class="dessertsContainer">
-      <div class="desserts">
+      <div class="buttonsLabel">
         <button 
-          class="btn-floating btn-large waves-effect waves-light cyan" 
+          class="btn-floating btn-large waves-effect waves-light cyan z-depth-4" 
           @click="handleClickFood('Desserts')"><i class="material-icons">cake</i></button>
+        <label class="labelClass">Desserts</label>
       </div>
     </div>
     <div class="waiterContainer">
-      <div class="waiter">
+      <div class="buttonsLabel">
         <button 
-          class="btn-floating btn-large waves-effect waves-light" 
+          class="btn-floating btn-large waves-effect waves-light z-depth-4" 
           @click="handleClickWaiter"><i class="material-icons">face</i></button>
+        <label class="labelClass">Waiter</label>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import WelcomeNav from './WelcomeNav.vue';
 export default {
   name: 'Welcome',
+  components: {
+    WelcomeNav,
+  },
+  data: function() {
+    return {
+      userImage: {
+        backgroundImage: `url(http://graph.facebook.com/${
+          this.$store.state.customer.id
+        }/picture)`,
+      },
+    };
+  },
   computed: {},
   methods: {
     handleClickFood(type) {
       this.$store.commit('menuSelected', type);
       this.$router.push('/Menu');
     },
+    goToUser() {
+      this.$router.push('/User');
+    },
     handleClickWaiter: () => {
-      M.toast({ html: 'The Waiter Is Coming :)' });
+      M.toast({
+        html: 'The Waiter Is Coming :)',
+        classes: 'rounded',
+      });
     },
   },
 };
--- src/store.js
+++ src/store.js
@@ -5,31 +5,41 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    customer: {},
-    menuSelected: '',
-    itemSelected: {},
+    customer: {}, // user name and id from facebook
+    menuSelected: '', // Food, Drinks or Desserts
+    authBoolean: false,
+    itemSelected: {}, // Dish or Drink selected for the Card Page
+    shoppingList: {}, // Dishes or Drinks ready to be order
     foodOptions: [
       {
         name: 'Pizza Napolitana',
         price: '12€',
+        description:
+          'I am a very simple card. I am good at containing small bits of information.',
         photo:
           'https://locosxlagastronomia.com/wp-content/uploads/2018/03/pizza-napolitana.jpg',
       },
       {
         name: 'Pasta Carbonara',
         price: '15€',
+        description:
+          'I am a very simple card. I am good at containing small bits of information.',
         photo:
           'https://pioneerwoman.files.wordpress.com/2016/04/how-to-nail-pasta-carbonara-00a.jpg',
       },
       {
         name: 'Pizza Napolitana',
         price: '12€',
+        description:
+          'I am a very simple card. I am good at containing small bits of information.',
         photo:
           'https://locosxlagastronomia.com/wp-content/uploads/2018/03/pizza-napolitana.jpg',
       },
       {
         name: 'Pasta Carbonara',
         price: '15€',
+        description:
+          'I am a very simple card. I am good at containing small bits of information.',
         photo:
           'https://pioneerwoman.files.wordpress.com/2016/04/how-to-nail-pasta-carbonara-00a.jpg',
       },
@@ -38,12 +48,16 @@ export default new Vuex.Store({
       {
         name: 'Guinness',
         price: '6€',
+        description:
+          'I am a very simple card. I am good at containing small bits of information.',
         photo:
           'https://www.guinness-storehouse.com/content/images/ygyw_images/thumbs/guinness_draught.jpg',
       },
       {
         name: 'Coca Cola',
         price: '3€',
+        description:
+          'I am a very simple card. I am good at containing small bits of information.',
         photo:
           'http://www.cocacolaespana.es/content/dam/journey/es/es/private/historia/love-coca-cola/2015/logo-coca-cola-lead.png',
       },
@@ -52,12 +66,16 @@ export default new Vuex.Store({
       {
         name: 'Chocolate Cake',
         price: '6€',
+        description:
+          'I am a very simple card. I am good at containing small bits of information.',
         photo:
           'https://img.taste.com.au/LHfi_7cR/w1200-h630-cfill/taste/2016/11/flourless-chocolate-hazelnut-cake-50137-1.jpeg',
       },
       {
         name: 'Cheesecake',
         price: '6€',
+        description:
+          'I am a very simple card. I am good at containing small bits of information.',
         photo:
           'https://driscolls.imgix.net/-/media/assets/recipes/raspberry-cheesecake-with-grand-marnier.ashx',
       },
@@ -72,6 +90,12 @@ export default new Vuex.Store({
     },
     customerInformation(state, info) {
       state.customer = info;
+    },
+    authBoolean(state) {
+      state.authBoolean = !state.authBoolean;
+    },
+    shoppingList(state, info) {
+      state.shoppingList = info;
     },
   },
   actions: {},
