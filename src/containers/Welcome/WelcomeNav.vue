a364c631b19637378c1e6d974187911d07393dac
BenjaKemp <BenjoKemp@gmail.com>
2018-06-27 18:14:48 +0200

    checkout with links

--- src/containers/Welcome/WelcomeNav.vue
+++ src/containers/Welcome/WelcomeNav.vue
@@ -3,15 +3,15 @@
     <div class="nav-wrapper">
       <h6 class="brand-logo center">Dumbwaiter</h6>
       <div class="left">
-        <a 
+        <a
           :style="props.userImage"
           class="btn-floating btn-large waves-effect waves-light imageTag z-depth-1"
           @click="props.goToUser"/>
       </div>
       <ul class="right">
         <li>
-          <button>
-            <i class="material-icons rightIcon">local_grocery_store</i>
+          <button @click="props.goCheckout">
+            <i class="material-icons rightIcon" >local_grocery_store</i>
           </button>
         </li>
       </ul>
