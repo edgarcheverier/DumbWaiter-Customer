4df9fed47afd2b31c515a0809df5178a106e2811
Viiralt <viiralt.h@gmail.com>
2018-06-28 15:47:00 +0200

    work on navbar

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  
+  <i 
+    class="medium material-icons">chevron_left</i>
+  
+
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  components: {},
+};
+</script>
+
+<style>
+#navbar-container {
+  height: 50px;
+}
+</style>
