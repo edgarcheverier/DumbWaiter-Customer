9adde40e35b0b4eed081636e9604a06b49b0e97a
BenjaKemp <BenjoKemp@gmail.com>
2018-06-28 15:01:27 +0200

    need shopping list

--- src/containers/Cart/ItemCart.vue
+++ src/containers/Cart/ItemCart.vue
@@ -3,6 +3,11 @@
     <div class="col s12 m6">
       <div class="card">
         <p>{{props.item.name}} - {{props.item.quantity}} - {{props.item.price}}</p>
+        <form>
+  <div class="value-button" id="decrease"  value="Decrease Value">-</div>
+  <input type="number" id="number" value="0" />
+  <div class="value-button" id="increase" value="Increase Value">+</div>
+</form>
       </div>
     </div>
   </div>
