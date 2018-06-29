bcce3fac53b74870558fed2dd7247c5a73fd8f47
BenjaKemp <BenjoKemp@gmail.com>
2018-06-29 12:07:08 +0200

    checkout no data

--- src/containers/Cart/ItemCart.vue
+++ src/containers/Cart/ItemCart.vue
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col s12 m6">
       <div class="card">
-        <p>{{props.item.name}} - {{props.item.price}}</p>
+        <p>{{props.item.name}} - {{props.item.price}}x{{props.item.quantity}} {{props.item.price*props.item.quantity}}</p>
         <form>
   <div class="value-button" id="decrease"  @click="props.quantityToggle(props.item.id, -1), props.subTotal()" value="Decrease Value">-</div>
   <div type="number">{{props.item.quantity}}</div>
