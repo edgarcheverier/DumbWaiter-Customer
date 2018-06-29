c53addff227381a5e967a8b1a19e59701d9af8e2
Viiralt <viiralt.h@gmail.com>
2018-06-29 11:37:32 +0200

    fix welcome screen

--- src/containers/Welcome/WelcomeNav.vue
+++ src/containers/Welcome/WelcomeNav.vue
@@ -1,20 +1,27 @@
 <template functional>
   <nav>
     <div class="nav-wrapper">
-      <h6 class="brand-logo center">Dumbwaiter</h6>
-      <div class="left">
-        <a
-          :style="props.userImage"
-          class="btn-floating btn-large waves-effect waves-light imageTag z-depth-1"
-          @click="props.goToUser"/>
-      </div>
-      <ul class="right">
-        <li>
-          <button @click="props.goCheckout">
-            <i class="material-icons rightIcon" >local_grocery_store</i>
-          </button>
-        </li>
-      </ul>
+      <h6>Dumbwaiter</h6>
+      <a
+        :style="props.userImage"
+        class="btn-floating btn-large waves-effect waves-light imageTag z-depth-1"
+        @click="props.goToUser"/>
     </div>
   </nav>
 </template>
+
+<style>
+.nav-wrapper {
+  height: 50px;
+  width: 90%;
+  margin: 0 auto;
+}
+h6 {
+  font-family: Raleway;
+  color: #fff;
+  width: 40px;
+  font-weight: 300;
+  font-size: 1.4em;
+  margin-left: 10px;
+}
+</style>
