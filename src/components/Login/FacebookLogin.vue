6267590fd40e41e52a9bae59e200f61a0f69070f
Viiralt <viiralt.h@gmail.com>
2018-07-03 13:37:38 +0200

    add login modularity, fix styling

---
+++ src/components/Login/FacebookLogin.vue
@@ -0,0 +1,104 @@
+<template>
+  <fb-signin-button
+    id="fb-login-button"
+    :params="fbSignInParams"
+    type="submit"
+    name="action"
+    @success="onSignInSuccess"
+    @error="onSignInError">
+    <ion-icon 
+      id="fb-login-icon" 
+      key="logo"
+      name="logo-facebook"/>
+    <p id="fb-login-text">Login with Facebook</p>
+  </fb-signin-button>
+</template>
+
+<script>
+import authService from '../../services/auth.service';
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: '246012439316213',
+    cookie: true,
+    xfbml: true,
+    version: 'v3.0',
+  });
+};
+(function(d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = '//connect.facebook.net/en_US/sdk.js';
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, 'script', 'facebook-jssdk');
+
+export default {
+  name: 'FacebookLogin',
+  data() {
+    return {
+      fbSignInParams: {
+        scope: 'email,public_profile',
+        return_scopes: true,
+      },
+    };
+  },
+  beforeCreate() {
+    const token = localStorage.getItem('token');
+    authService.verifyToken(token, response => {
+      if (response.isvalid) {
+        this.$store.commit(
+          'customerInformation',
+          response.user
+        );
+        this.$router.push('/maps');
+      }
+    });
+  },
+  methods: {
+    async onSignInSuccess(response) {
+      const authFacebook = await authService.authFacebook(
+        response.authResponse,
+        response => {
+          this.$store.commit(
+            'customerInformation',
+            response.user
+          );
+          localStorage.setItem('token', response.token);
+          this.$router.push('/Maps');
+        }
+      );
+    },
+    onSignInError(error) {
+      console.log('login failed', error);
+    },
+  },
+};
+</script>
+
+<style scoped>
+#fb-login-button {
+  width: 100%;
+  height: 60px;
+  display: flex;
+  color: #ffffff;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: #3b5998;
+}
+#fb-login-icon {
+  width: 2.5em;
+  height: 2.5em;
+  margin-right: 20px;
+}
+#fb-login-text {
+  font-size: 1em;
+  font-weight: 400;
+}
+@import '../../assets/styles/global.css';
+</style>
